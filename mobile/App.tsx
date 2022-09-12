ea6fb0bb67d6d6d269073d4a5bc01d98c38be1db
Daniel Pinheiro <daniel.pin@outlook.com>
2022-09-12 20:57:44 -0300

    ajustes mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,34 +1,6 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
+import { Background } from "./src/components/Background";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <Button title="Send 1" />
-      <Button title="Send 2" />
-      <Button title="Send 3" />
-      <Button title="Hello World" />
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Background></Background>;
 }
-interface ButtonProps {
-  title: string;
-}
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text>{props.title}</Text>
-    </TouchableOpacity>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
