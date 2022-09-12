ac2914be0549eb38a0c2b1826e94e69a3d0942c8
Daniel Pinheiro <daniel.pin@outlook.com>
2022-09-12 19:31:17 -0300

    configurando tailwind

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,7 @@
-interface ButtonProps {
-  title: string;
-}
-
-function Button(props: ButtonProps) {
-  return <button>{props.title}</button>;
-}
+import "./styles/main.css";
 
 function App() {
-  return (
-    <div>
-      <Button title="Send 1"></Button>
-      <Button title="Send 2"></Button>
-      <Button title="Send 3"></Button>
-      <Button title="Hello World"></Button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
