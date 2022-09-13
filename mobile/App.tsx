5e7b31f40f76bcafcac50a2d2616663cc9fa82ea
Daniel Pinheiro <daniel.pin@outlook.com>
2022-09-12 23:28:04 -0300

    Finalização mobile dia 02

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,32 @@
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
 import { Background } from "./src/components/Background";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  return <Background></Background>;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
+  );
 }
